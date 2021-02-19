f67440b7079bb923194a28219c408cd95bb42e69
bcollard <bcollard@callfire.com>
2021-02-19 10:39:06 -0800

    eslint fix

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-const app = require('./server.js');
 const mongoose = require('mongoose');
+const app = require('./server.js');
 
 mongoose.connect(process.env.DATABASE_URL, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true,
-   useFindAndModify: false,
-   useCreateIndex: true,
- }).then(() => console.log('Connected to MongoDB'));
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+}).then(() => console.log('Connected to MongoDB'));
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server running on port ${port}`));
